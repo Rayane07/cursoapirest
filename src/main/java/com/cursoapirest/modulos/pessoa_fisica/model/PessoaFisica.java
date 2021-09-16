84e58b2f2baaa5aa93a5888c484ec55b4a85a247
Rayane07 <rayaneteles.tst@hotmail.com>
2021-09-16 14:57:12 -0300

    add pessoa fisica

---
+++ src/main/java/com/cursoapirest/modulos/pessoa_fisica/model/PessoaFisica.java
@@ -0,0 +1,73 @@
+package com.cursoapirest.modulos.pessoa_fisica.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class PessoaFisica implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nome;
+    private String email;
+   
+
+    public PessoaFisica(){}
+
+    public PessoaFisica(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PessoaFisica)) return false;
+        PessoaFisica that = (PessoaFisica) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/cursoapirest/modulos/pessoa_fisica/rest/PessoaFisicaApi.java
@@ -0,0 +1,10 @@
+package com.cursoapirest.modulos.pessoa_fisica.rest;
+import com.cursoapirest.modulos.pessoa_fisica.model.PessoaFisica;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface PessoaFisicaApi {
+
+    ResponseEntity<List<PessoaFisica>> ListarPessoas();
+}
